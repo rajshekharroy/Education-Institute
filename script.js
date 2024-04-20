027e4a3a58ba1689a948e870a54280369fd1738e
Raj <156524418+rajshekhar34@users.noreply.github.com>
2024-04-20 11:43:24 +0530

    "Added 'exclude' class to certain elements to control animation visibility; adjusted animations for better scrolling experience; updated media query for better responsiveness on smaller screens"

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const hamburger = document.querySelector(".hamburger");
 const sidebar = document.querySelector('.sidebar')
 const sidebar_header = document.querySelector('.sidebar-header');
 
+// const blog = document.querySelectorAll('.blog')
+
+
+
+
+
+
 xmark.addEventListener("click" , ()=>{
     sidebar.classList.add('sidebar-hidden')
     sidebar_header.classList.add('sidebar-header-hidden')
@@ -17,6 +24,29 @@ hamburger.addEventListener("click", ()=>{
     sidebar_header.classList.remove('sidebar-header-hidden')
     sidebar.classList.remove('sidebar-none')
 })
+
+
+const elements = document.querySelectorAll('h1:not(.exclude),h4,.feature-img-div,button,.blog,p,.course-content');
+
+
+
+const allAnim = () => {
+    const triggerBottom = (window.innerHeight);
+    elements.forEach((element)=>{
+        const boxtop = element.getBoundingClientRect().top;
+        const boxbottom = element.getBoundingClientRect().bottom
+        if (boxtop < triggerBottom && boxbottom > 200) {
+            element.style.opacity = "1";
+            element.style.transform = "translateY(0)";
+            element.style.scale = "1"
+        }
+    })
+        
+};
+window.addEventListener('scroll', allAnim);
+
+
+
 
 
 
