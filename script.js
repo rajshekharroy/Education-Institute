918a6eb40054944c73540c72b04db5fefd2f9bf5
Raj <156524418+rajshekhar34@users.noreply.github.com>
2024-04-24 08:43:59 +0530

    "Added responsiveness, fixed bugs, and improved styling in various sections; added scroll to top button and scroll animation; updated fonts and colors"

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const xmark = document.querySelector(".xmark");
-const hamburger = document.querySelector(".hamburger");
+const xmark_sidebar = document.querySelector(".xmark,.sidebar");
+const hamburger = document.querySelectorAll(".hamburger");
 const sidebar = document.querySelector(".sidebar");
 const sidebar_header = document.querySelector(".sidebar-header");
 const course_expand = document.querySelector(".course-expand");
 const expand = document.querySelector(".expand");
 const sidebar_sublist = document.querySelector(".sidebar-sublist");
-
-// const blog = document.querySelectorAll('.blog')
+const header = document.querySelector('.header');
+// const down_arrow = document.querySelector('.down-arrow');
 
 course_expand.addEventListener("click", () => {
   expand.classList.toggle("fa-plus");
@@ -14,20 +14,23 @@ course_expand.addEventListener("click", () => {
   sidebar_sublist.classList.toggle("hide");
 });
 
-xmark.addEventListener("click", () => {
+xmark_sidebar.addEventListener("click", () => {
   sidebar.classList.add("sidebar-hidden");
   sidebar_header.classList.add("sidebar-header-hidden");
 });
+hamburger.forEach((ham)=>{
+  ham.addEventListener("click", () => {
+    sidebar.classList.remove("sidebar-hidden");
+    sidebar_header.classList.remove("sidebar-header-hidden");
+  });
+})
 
-hamburger.addEventListener("click", () => {
-  sidebar.classList.remove("sidebar-hidden");
-  sidebar_header.classList.remove("sidebar-header-hidden");
-});
 
 const elements = document.querySelectorAll(
-  // "h1:not(.exclude),h4,.feature-img-div,button,.blog,p,.course-content"
-  ".course,.login,.whatsapp,#first,.course1,.course2,.course3,.cca,.com,.dom,#testimonial-ad,.feature-img-div,.blog,.course-content,button,h1,h2,h3,h4,p"
+  ".course,.login,.whatsapp,#first,#second,.course-container,.cca,.com,.dom,#testimonial-ad,.feature-img-div,.blog,.course-content,button,h1,h2,h3,h4,p"
 );
+
+
 
 const allAnim = () => {
   elements.forEach((element) => {
@@ -42,3 +45,53 @@ const allAnim = () => {
 };
 window.addEventListener("load", allAnim);
 window.addEventListener("scroll", allAnim);
+
+
+window.addEventListener('scroll', function() {
+  let scrollToTopButton = document.querySelector('.gototop-section');
+  if (window.scrollY > 0) {
+    scrollToTopButton.style.visibility = 'visible';
+    scrollToTopButton.style.transform = 'translateY(0)'
+    scrollToTopButton.style.opacity = '1';
+  } else {
+    scrollToTopButton.style.transform = 'translateY(100px)'
+    scrollToTopButton.style.visibility = 'hidden';
+    scrollToTopButton.style.opacity = '0';
+  }
+});
+
+// down_arrow.addEventListener("click", () => {
+//   window.scrollTo({
+//       top: document.documentElement.clientHeight,
+//       behavior: "smooth"
+//   });
+// });
+
+window.addEventListener("scroll", function () {
+  let scrollTop = document.documentElement.scrollTop;
+  let scrollHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let progress = (scrollTop / scrollHeight) * 135;
+  document.getElementById("svg-path").style.strokeDashoffset =
+    135 - progress;
+
+    document.querySelector('.first-circle').addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' 
+  });
+});
+
+});
+
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        header.style.transform = "translateY(0)";
+    }else{
+      header.style.transform = "translateY(-100px)";
+    }
+});
+
+
